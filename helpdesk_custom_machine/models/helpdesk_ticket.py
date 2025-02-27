9bf0c85589fc1fe5aa429d8979818ffe1549556f
CarlosAdrianM <carlosadrian@nuevavision.es>
2025-02-27 17:51:26 +0000

    Helpdesk machine permite filtrar por serial_number

--- helpdesk_custom_machine/models/helpdesk_ticket.py
+++ helpdesk_custom_machine/models/helpdesk_ticket.py
@@ -1,4 +1,7 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
@@ -11,5 +14,44 @@ class HelpdeskTicket(models.Model):
     serial_number = fields.Char(
         related='machine_id.serial_number',  # Campo relacionado
         string='Serial Number',              # Etiqueta del campo
-        readonly=True                        # Solo lectura
+        readonly=True,                       # Solo lectura
+        store=True,  
+        index=True
     )
+
+    def _modify_search_domain(self, domain):
+        """
+        Método auxiliar para modificar el dominio de búsqueda e incluir el campo serial_number.
+        """
+        new_domain = []
+        for arg in domain:
+            if isinstance(arg, (list, tuple)) and len(arg) == 3:
+                field, operator, value = arg
+                # Si estamos buscando por nombre o número y el valor podría ser un número de serie
+                if field in ['name', 'number'] and value and isinstance(value, str):
+                    new_domain.extend(['|',
+                        ('serial_number', 'ilike', value),
+                        (field, operator, value)
+                    ])
+                else:
+                    new_domain.append(arg)
+            else:
+                new_domain.append(arg)
+        
+        _logger.info("Dominio de búsqueda modificado: %s", new_domain)
+        return new_domain
+
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        """Sobreescribimos _search para incluir búsquedas por serial_number"""
+        new_args = self._modify_search_domain(args)
+        return super()._search(new_args, offset=offset, limit=limit, order=order, 
+                            count=count, access_rights_uid=access_rights_uid)
+
+    @api.model
+    def web_read_group(self, domain, fields, groupby, limit=None, offset=0, orderby=False, lazy=True, **kwargs):
+        """
+        Sobrescribimos web_read_group para incluir búsquedas por serial_number.
+        """
+        new_domain = self._modify_search_domain(domain)
+        return super().web_read_group(new_domain, fields, groupby, limit=limit, offset=offset, orderby=orderby, lazy=lazy)
